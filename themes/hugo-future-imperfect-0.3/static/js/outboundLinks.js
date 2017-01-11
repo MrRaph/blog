453904fae4e8ed967ffb0233a85230183540efb6
MrRaph_ <MrRaph@users.noreply.github.com>
2017-01-11 23:39:13 +0100

    Create outboundLinks.js

---
+++ themes/hugo-future-imperfect-0.3/static/js/outboundLinks.js
@@ -0,0 +1,14 @@
+<script>
+/**
+* Fonction de suivi des clics sur des liens sortants dans Analytics
+* Cette fonction utilise une chaîne d'URL valide comme argument et se sert de cette chaîne d'URL
+* comme libellé d'événement. Configurer la méthode de transport sur 'beacon' permet d'envoyer le clic
+* au moyen de 'navigator.sendBeacon' dans les navigateurs compatibles.
+*/
+var trackOutboundLink = function(url) {
+   ga('send', 'event', 'outbound', 'click', url, {
+     'transport': 'beacon',
+     'hitCallback': function(){document.location = url;}
+   });
+}
+</script>
